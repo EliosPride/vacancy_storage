243aeab6cf0ae49c8697f08671da901de8d4badd
Illia Sydorchuk <batonvagon@gmail.com>
2023-05-03 13:59:50 +0300

    Initial commit

---
+++ src/main/java/com/elios/pride/vacancy_storage/VacancyStorageApplication.java
@@ -0,0 +1,13 @@
+package com.elios.pride.vacancy_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elios/pride/vacancy_storage/VacancyStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elios.pride.vacancy_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
